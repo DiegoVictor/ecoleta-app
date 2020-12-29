9701d277c1d1dc2abfc88a438126e0b5fea7a086
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-12-29 18:15:21 -0300

    Update main entry point

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import { AppLoading } from 'expo';
+import AppLoading from 'expo-app-loading';
 import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto';
 import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu';
 
