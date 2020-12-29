7c423a72f384ec3331be2c7d61701b8cfa58dbbe
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-12-29 18:15:49 -0300

    Configure dotenv

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,19 @@
-// eslint-disable-next-line func-names
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'dotenv-import',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: false,
+        },
+      ],
+    ],
   };
 };
