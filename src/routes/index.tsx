c118b8689eacdec796b585f7500ae52380692924
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-12-30 18:57:59 -0300

    Update routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Home from './pages/Home';
-import Points from './pages/Points';
-import Detail from './pages/Detail';
+import Home from '../pages/Home';
+import Points from '../pages/Points';
+import Detail from '../pages/Detail';
 
 const AppStack = createStackNavigator();
 
