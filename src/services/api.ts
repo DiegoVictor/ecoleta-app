60006cd76cbc3f747a192beb32151987b54c8d2b
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-12-31 10:55:20 -0300

    Read from env

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,5 @@
 import axios from 'axios';
-import Constants from 'expo-constants';
-
-const { API_URL } = Constants.manifest.extra;
+import { API_URL } from '@env';
 
 export default axios.create({
   baseURL: API_URL,
