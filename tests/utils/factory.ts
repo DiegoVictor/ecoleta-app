ced744c846aa47f47ca88e4abea082bf1715fdf5
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-01-03 15:30:22 -0300

    Create factories

---
+++ tests/utils/factory.ts
@@ -0,0 +1,37 @@
+import faker from 'faker';
+import factory from 'factory-girl';
+
+factory.define(
+  'Point',
+  {},
+  {
+    point: {
+      id: () => String(faker.random.number()),
+      name: faker.name.findName,
+      city: faker.address.city,
+      uf: faker.address.state,
+      email: faker.internet.email,
+      whatsapp: faker.phone.phoneNumber,
+      image_url: faker.image.imageUrl,
+      latitude: () => Number(faker.address.latitude()),
+      longitude: () => Number(faker.address.longitude()),
+    },
+    items: [
+      {
+        title: faker.random.word(),
+      },
+    ],
+  },
+);
+
+factory.define(
+  'Item',
+  {},
+  {
+    id: () => String(faker.random.number()),
+    title: faker.random.word,
+    image_url: faker.image.imageUrl,
+  },
+);
+
+export default factory;
